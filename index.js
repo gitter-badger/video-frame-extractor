81810f337af3290cde5f20c7d00a2bc97044175d
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2015-01-19 13:42:08 +0200

    more changes

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var exec = require('child_process').exec;
 
+
+function calculateFrame(frameRate, time, frameNumber) {
+  var hours, minutes, seconds, hhmmss, frames;
+  
+  hhmmss = time.split(':');
+ 
+  hours = hhmmss[0];
+  minutes = hhmmss[1];
+  seconds = hhmmss[2];
+
+  seconds = (+hours) * 60 * 60 + (+minutes) * 60 + (+seconds); 
+  return (seconds * frameRate) + frameNumber -1
+}
+
 exports.extractFrame = function(filePath, frameRate, time, frameNumber) {
-  console.log('Extracting Frame: ' + time + ';' + frameNumber);
-  command  = 'ffmpeg  -vsync passthrough  -ss ' + time + ' -i ' + filePath +' -vf "select=gte(n\\, ' + frameNumber  +')" -vframes 1 ' + frameNumber + '.png -y';
+  var frame, command;
+  
+  frame = calculateFrame(frameRate, time, frameNumber);
+  console.log(time);
+  command  = 'ffmpeg -vsync passthrough -i ' + filePath +' -vf "select=gte(n\\, ' + frame  +')" -vframes 1 ' + frameNumber + '.png -y';
   child = exec(command, function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
