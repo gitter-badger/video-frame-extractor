d7417c3d4ff7d6fb3f31020d8d0a4137ff6c7cd5
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2015-01-19 13:35:08 +0200

    updates to ffmpeg command

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 var exec = require('child_process').exec;
 
-
-function calculateFrame(frameRate, time, frameNumber) {
-  var hours, minutes, seconds, hhmmss, frames;
-  
-  hhmmss = time.split(':');
- 
-  hours = hhmmss[0];
-  minutes = hhmmss[1];
-  seconds = hhmmss[2];
-
-  seconds = (+hours) * 60 * 60 + (+minutes) * 60 + (+seconds); 
-  return (seconds * frameRate) + frameNumber;
-}
-
 exports.extractFrame = function(filePath, frameRate, time, frameNumber) {
-  var frame, command;
-  
-  frame = calculateFrame(frameRate, time, frameNumber);
-  command  = 'ffmpeg -r 1 -i ' + filePath +' -vf "select=gte(n\\, ' + frame  +')" -vframes 1 ' + frameNumber + '.png -y';
-
+  console.log('Extracting Frame: ' + time + ';' + frameNumber);
+  command  = 'ffmpeg  -vsync passthrough  -ss ' + time + ' -i ' + filePath +' -vf "select=gte(n\\, ' + frameNumber  +')" -vframes 1 ' + frameNumber + '.png -y';
   child = exec(command, function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
