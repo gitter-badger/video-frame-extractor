5ae7154e89c5d9247708e1c0141a1f28e1296e9d
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2015-01-19 12:25:39 +0200

    cleanup

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ var exec = require('child_process').exec;
 
 
 function calculateFrame(frameRate, time, frameNumber) {
-  var a = time.split(':'); // split it at the colons
-
-  // minutes are worth 60 seconds. Hours are worth 60 minutes.
-  var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]); 
+  var hours, minutes, seconds, hhmmss, frames;
   
-  return Math.ceil((seconds * frameRate) + frameNumber - 1);
+  hhmmss = time.split(':');
+ 
+  hours = hhmmss[0];
+  minutes = hhmmss[1];
+  seconds = hhmmss[2];
+
+  seconds = (+hours) * 60 * 60 + (+minutes) * 60 + (+seconds); 
+  return parseFloat((seconds * frameRate)) + frameNumber;
 }
 
 exports.extractFrame = function(filePath, frameRate, time, frameNumber) {
   var frame = calculateFrame(frameRate, time, frameNumber);
-  var command  = 'ffmpeg -r ' + frameRate + ' -i ./fixtures/timecode.mp4 -vf "select=gte(n\\, ' + frame +')" -vframes 1 ' + frameNumber + '.png -y';
-  console.log(command);
+  console.log(frame);
+  var command  = 'ffmpeg -r 1 -i ./fixtures/timecode.mp4 -vf "select=gte(n\\, ' + frame  +')" -vframes 1 ' + frameNumber + '.png -y';
 
   child = exec(command, function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
     if (error !== null) {
       console.log('exec error: ' + error);
     }
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 var videoFrameExtractor = require('../index');
 
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 10; i++) {
   videoFrameExtractor.extractFrame('fixtures/timecode.mp4' , 29.970, '00:05:00', i);
 }
